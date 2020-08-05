ff70645643d2574f1dbef71b31bd28baaf915de6
Noelle Crawford <noellegc3@gmail.com>
2020-08-05 15:19:45 -0700

    Nicer looking buttons

--- components/NavButtons.js
+++ components/NavButtons.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native'
 
 export default class NavButtons extends React.Component {
   constructor(props) {
@@ -8,22 +8,67 @@ export default class NavButtons extends React.Component {
   }
 
   render() {
-    return (
-      <TouchableOpacity
-        style={styles.button}
-      >
-        <Text>Hello</Text>
-      </TouchableOpacity>
-    )
+    if(this.state.open) {
+      return (
+        <View style={styles.container}>
+          <TouchableOpacity
+            style={styles.button}
+          >
+            <Text>For You</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+          >
+            <Text>Browse</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+          >
+            <Text>Settings</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.setState({open: false})}
+          >
+            <Text>Close</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.container}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.setState({open: true})}
+          >
+            <Text>Open</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    }
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+
   button: {
-    width: 100,
-    height: 100,
+    width: 50,
+    height: 50,
     backgroundColor: '#58111a',
     borderRadius: 100,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 })
 

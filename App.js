2f8c5627f3ce1e972456b5849cd91d204e83667a
Noelle Crawford <noellegc3@gmail.com>
2020-08-05 15:20:52 -0700

    moved home screen to its own file

--- App.js
+++ App.js
@@ -3,20 +3,8 @@ import { View, Text, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import NavButtons from './components/NavButtons.js'
-import Movie from './components/Movie.js'
-
-class HomeScreen extends React.Component {
-  render() { 
-  return (
-    <View>
-      <Text>Home Screen</Text>
-      <NavButtons />
-      <Movie />
-    </View>
-  );
-  }
-}
+import HomeScreen from './screens/HomeScreen.js'
+import MovieBrowser from './screens/MovieBrowser.js'
 
 const Stack = createStackNavigator();
 
@@ -25,10 +13,17 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Watch Next" component={HomeScreen} />
-        {/* <Stack.Screen name="Browse" component={MovieBrowser} /> */}
+        <Stack.Screen name="Browse" component={MovieBrowser} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+  },
+})
 
 export default App;
